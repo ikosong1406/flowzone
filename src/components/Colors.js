e52941ac0324ec05766c39eb919d258b91179dc4
Alexandra <alexandervirtuous14@gmail.com>
2024-05-21 16:41:01 +0100

    modified:   src/components/Colors.js

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,11 +1,5 @@
 const Colors = {
-  green: "#9457eb ",
-  blue: "#0038a8",
-  black: "#3b3c3d",
-  white: "#e8eaeb",
-  red: "#dc2649",
-  indigo: "#510560",
-  violet: "#253745",
+  violet: "#8a24dd",
 };
 
 export default Colors;
